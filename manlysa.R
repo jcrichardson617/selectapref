854d9358a859d1b204a3141d9418cc6b45b69fb1
jcrichardson617 <jcrichardson617@gmail.com>
2017-04-24 15:22:47 -0400

    Add files via upload

--- manlysa.R
+++ manlysa.R
@@ -1,4 +1,4 @@
-#' Manly's alpha feeding preference (Chesson 196X).
+#' Manly's alpha feeding preference (Chesson 1968).
 #' Returns Manly'as alpha index from vectors of initial and final food item counts.
 #' @param initial A vector of initial food items counts available to the organism in the environment
 #' @param consumed A vector of food items consumed by the organism
@@ -6,11 +6,21 @@
 #' @param perc Converts values to percentages? Defaults to FALSE.
 #' @param deplete For use in experiments where food sources deplete? Defaults to TRUE.
 #' @keywords Manly's alpha preference selectivity
-
+#'
+#' @examples
+#' initial_prey_count <- c(10,10,10,10,10,10)
+#' number_prey_consumed <- c(9,8,1,3,5,9)
+#' manlysalpha(initial = initial_prey_count, consumed = number_prey_consumed,
+#' stand = TRUE, perc = FALSE, deplete = TRUE)
+#'
+#' manlysalpha(initial = initial_prey_count, consumed = number_prey_consumed,
+#' stand = TRUE, perc = TRUE, deplete = TRUE)
+#'
+#' @export
 manlysalpha <- function (initial, consumed, stand = FALSE, perc = FALSE, deplete = TRUE) {
   maconst <- ((consumed/initial)/(sum(consumed/initial)))
   madepl <- ((log((initial-consumed)/initial))/(sum(log((initial-consumed)/initial))))
-  
+
   if(deplete == TRUE) {
     if(perc == TRUE) {
       if(stand == TRUE) {
--- selectivity.R
+++ selectivity.R
@@ -4,13 +4,17 @@
 #' @param available A vector of food items available to the organism in the environment
 #' @param consumed A vector of food items consumed by the organism
 #' @keywords forage ratio
-#' 
-#' 
-
+#'
+#' @examples
+#' availableprey <- c(10,10,10,10,10)
+#' consumedprey <- c(9,0,0,1,5)
+#' FR(available = availableprey, consumed = consumedprey)
+#'
+#' @export
 FR <- function(available, consumed) {
   r <- consumed/sum(consumed)
   p <- available/sum(available)
-  
+
   return (r/p)
 }
 
@@ -20,11 +24,18 @@ FR <- function(available, consumed) {
 #' @param consumed A vector of food items consumed by the organism
 #' @param jacob Converts to Jacob's electivity index? Defaults to FALSE.
 #' @keywords ivlev selectivity
-
+#'
+#' @examples
+#' availableprey <- c(10,10,10,10,10)
+#' consumedprey <- c(9,0,0,1,5)
+#' ivlev(available = availableprey, consumed = consumedprey, jacob = FALSE)
+#' ivlev(available = availableprey, consumed = consumedprey, jacob = TRUE)
+#'
+#' @export
 ivlev <- function(available, consumed, jacob = FALSE) {
   r <- consumed/sum(consumed)
   p <- available/sum(available)
-  
+
   if(jacob == TRUE) {
     return (r-p)/(r+p-(2*r*p))
   } else {
